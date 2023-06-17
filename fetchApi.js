3e60d7e42199d005fb497a1eeab2adbd360215a4
nathanaelpganata <nathanaelpg8@gmail.com>
2023-06-17 11:16:28 +0700

    feat: add fetch_api

---
+++ fetchApi.js
@@ -0,0 +1,8 @@
+const fetchData = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await response.json();
+
+    console.log(data);
+};
+
+fetchData();
